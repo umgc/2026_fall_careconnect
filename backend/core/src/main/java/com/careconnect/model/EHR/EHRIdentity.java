3d780bde247dd4c4ee3a1aee294b2110d61a133a
Max <maxstine@gmail.com>
2026-09-10 23:31:02 -0400

    Progress Save

--- backend/core/src/main/java/com/careconnect/model/EHR/EHRIdentity.java
+++ backend/core/src/main/java/com/careconnect/model/EHR/EHRIdentity.java
@@ -1,3 +1,23 @@
+package com.careconnect.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import org.hl7.fhir.r4.model.Patient;
+
+
+
+@Entity
+@Table(name = "ehr_identities")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 class EHRIdentity{
+
 
 }
